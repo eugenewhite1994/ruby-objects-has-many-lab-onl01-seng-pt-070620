6b97eecadb8dc5c25ee5a9d9a572f31e29be756b
Eugene White <eugenewhite1994@gmail.com>
2020-08-06 23:58:29 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,23 +1,18 @@
 class Post
   attr_accessor :title, :author
-
   @@all = []
-
-  def initialize(title, author=nil)
+  def initialize(title)
     @title = title
-    @author = author
     @@all << self
   end
-
-  def author_name
-    if self.author
-      self.author.name
-    else
-      nil
-    end
-  end
-
   def self.all
     @@all
   end
+def author_name
+  if @author == nil
+    nil
+  else
+    @author.name
+  end
+end
 end
